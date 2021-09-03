9d6afe3bd96e2dfd13dab5a85aad4ae5f758062b
moohbr <48070411+moohbr@users.noreply.github.com>
2021-09-03 11:55:23 -0300

    Delete escrita.c

--- escrita.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void escrever_rank(nome){
-    char* nome;
-    //int pontuacao;
-    FILE *f  = fopen("rank.txt","a");
-    fprintf(f, "\n");
-    fprintf(f,"%s", nome);
-    fclose(f);
-}
